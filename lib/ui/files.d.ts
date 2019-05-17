9bbf8119a124bef7de2afe0164e7c6bb3019839d
tjb346 <tbrockhoeft@texastribune.org>
2019-05-17 11:29:47 -0500

    update docs

--- lib/ui/files.d.ts
+++ lib/ui/files.d.ts
@@ -1,5 +1,9 @@
 import { CustomElement } from "elements/lib/element.js";
 import { Directory } from "../files/base.js";
+/**
+ * An custom element that represents a {@link Directory} instance. Can be added as a
+ * child of {@link FileBrowser} to browse the directory in an HTML document.
+ */
 export declare abstract class DirectoryElement extends CustomElement {
     protected abstract readonly directory: Directory;
     refresh(): void;
--- src/ui/files.ts
+++ src/ui/files.ts
@@ -3,6 +3,10 @@ import {Directory} from "../files/base.js";
 import {FileBrowser} from "./browser.js";
 
 
+/**
+ * An custom element that represents a {@link Directory} instance. Can be added as a
+ * child of {@link FileBrowser} to browse the directory in an HTML document.
+ */
 export abstract class DirectoryElement extends CustomElement {
     protected abstract readonly directory : Directory;
 
