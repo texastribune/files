2660f6e30fa629e2cbec926cc9038c744f0924ff
Jonathan Peacher <djpeacher@gmail.com>
2022-09-08 13:27:00 -0500

    removed watch in favor of makefile.

--- docs/webpack.config.js
+++ docs/webpack.config.js
@@ -2,10 +2,9 @@ var path = require('path');
 
 module.exports = {
   mode: 'development',
-  watch: true,
   entry: path.resolve(__dirname, 'index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+    filename: 'bundle.js',
+  },
 };
