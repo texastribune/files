e667167e530aa5f0c143c9d1c8a3cd08f0e8e8d5
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2022-09-15 11:21:07 -0500

    Remove hardcoded fill

--- src/ui/icons.ts
+++ src/ui/icons.ts
@@ -38,7 +38,7 @@ export const documentIcon =
 
   export const upLevelIcon = 
   `<svg xmlns="http://www.w3.org/2000/svg" version="1.0" width="220" height="220" viewBox="0 0 200 200" preserveAspectRatio="xMidYMid meet">
-    <g transform="translate(0.000000,200.000000) scale(0.100000,-0.100000)" fill="#000000" stroke="none">
+    <g transform="translate(0.000000,200.000000) scale(0.100000,-0.100000)" stroke="none">
     <path d="M120 1000 l0 -780 880 0 880 0 -2 662 -3 663 -575 3 -575 2 -115 115 -115 115 -187 0 -188 0 0 -780z m908 186 c15 -8 69 -57 120 -109 98 -100 111 -130 75 -169 -27 -30 -60 -22 -113 28 l-50 46 -2 -193 -3 -193 -27 -16 c-25 -13 -31 -13 -55 0 l-28 16 -3 193 -2 193 -50 -46 c-28 -26 -59 -46 -71 -46 -32 0 -59 29 -59 63 0 24 18 48 98 129 81 84 115 112 140 117 2 1 15 -5 30 -13z"/>
     </g>
   </svg>`;
